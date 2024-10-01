ab9f051a6d7742a36520ec27f724a7986f5294d4
PSantosss <josepaolo.santos.cics@ust.edu.ph>
2024-10-01 12:03:09 +0800

    helloo

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,36 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PostController;
 
-Route::middleware(['auth', 'admin'])->group(function () {
+// Admin Routes - Only accessible to admins
+Route::middleware(['auth', 'is_admin'])->group(function () {
+    // Admin posts routes
     Route::get('/admin/posts', [PostController::class, 'list'])->name('admin.posts.list');
     Route::get('/admin/posts/create', [PostController::class, 'create'])->name('admin.posts.create');
     Route::get('/admin/posts/{post}/edit', [PostController::class, 'edit'])->name('admin.posts.edit');
+
+    // Admin user management routes
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
 });
 
-Route::middleware(['auth', 'user'])->group(function () {
+// Authenticated User Routes
+Route::middleware(['auth'])->group(function () {
+    // Regular user post routes
     Route::get('/user/posts/{post}', [PostController::class, 'view'])->name('user.posts.view');
     Route::get('/user/posts/create', [PostController::class, 'staticForm'])->name('user.posts.create');
+
+    // Dashboard
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    // Page 6 and Page 7 routes
+    Route::get('/page6', [PageController::class, 'page6'])->name('page6');
+    Route::get('/page7/{id}', [PageController::class, 'page7'])->name('page7');
 });
 
 // Public Pages
@@ -40,28 +61,3 @@ Route::get('/page4', function () {
 Route::get('/page5', function () {
     return view('pages.page5');
 })->name('page5');
-
-
-// Authenticated Routes
-Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard'); // Ensure this view exists
-    })->name('dashboard');
-
-    Route::get('/page6', [PageController::class, 'page6'])->name('page6');
-    Route::get('/page7/{id}', [PageController::class, 'page7'])->name('page7');
-
-
-});
-
-
-// Route to list all users
-Route::middleware(['auth', 'is_admin'])->group(function () {
-    Route::get('/users', [UserController::class, 'index'])->name('users.index');
-    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-    Route::post('/users', [UserController::class, 'store'])->name('users.store');
-    Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
-    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-    Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-});
-
